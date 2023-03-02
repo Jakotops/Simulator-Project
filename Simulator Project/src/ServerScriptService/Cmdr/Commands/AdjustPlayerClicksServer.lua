215b7b87939f819826947ea216933fb32b9178e5
Jakotops <79404345+Jakotops@users.noreply.github.com>
2023-03-02 15:50:20 +0000

    Remove junk

--- Simulator Project/src/ServerScriptService/Cmdr/Commands/AdjustPlayerClicksServer.lua
+++
@@ -1,8 +0,0 @@
-local ServerScriptService = game:GetService("ServerScriptService")
-
-local PlayerData = require(ServerScriptService.PlayerData.Manager)
-
-return function (context, player: Player, amount: number)
-	PlayerData.AdjustClicks(player, amount)
-	return "Adjusted"
-end
